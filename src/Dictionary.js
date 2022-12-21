aa3f7b8b40eb0afe74a9890822ade217b697968c
wiebkerichter <109521766+wiebkerichter@users.noreply.github.com>
2022-12-21 16:39:11 +0100

    installed axios, and added axios request

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -11,6 +12,13 @@ export default function Dictionary() {
   function updateKeyword(event) {
     setKeyword(event.target.value);
   }
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+  // documentation: https://dictionaryapi.dev/
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  axios.get(url).then(handleResponse);
 
   return (
     <form onSubmit={handleSubmit}>
